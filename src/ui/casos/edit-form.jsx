fe63428e2c194ed0537b1ac2281a952aa2f9fe35
Jhonathan Velez <kokoa.arte@gmail.com>
2025-09-02 09:36:42 -0500

    updaye commit

--- src/ui/casos/edit-form.jsx
+++ src/ui/casos/edit-form.jsx
@@ -913,7 +913,7 @@ export default function EditCasesForm({ cases }) {
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200">
           Cancelar
         </Link>
-        <Button type="submit">Crear sucursal</Button>
+        <Button type="submit">Actualizar Casos</Button>
       </div>
     </form>
   );
