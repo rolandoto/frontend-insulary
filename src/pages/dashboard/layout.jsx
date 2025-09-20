b11c8137791078ff28c5fe3505fa4afd355b9096
Jhonathan Velez <kokoa.arte@gmail.com>
2025-09-20 15:45:36 -0500

    update commit

--- src/pages/dashboard/layout.jsx
+++ src/pages/dashboard/layout.jsx
@@ -2,11 +2,11 @@ import SideNav from "../../ui/dashboard/sidenav";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="flex  flex-col md:flex-row ">
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6  md:p-12">{children}</div>
     </div>
   );
 }
